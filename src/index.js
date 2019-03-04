d48621ef15bf1359d16fc5bd3f82517080be274c
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-03-04 19:02:49 +0300

    feat: implement the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  
+  // get the base number multipliers
+  let i = 2
+  let multipliers = {}
+  while ( base !== 1  ){
+    if ( base % i === 0 ){
+      if ( multipliers.hasOwnProperty(i) ) multipliers[i] += 1
+      else multipliers[i] = 1
+
+      base /= i
+      i = 2
+    }
+    else i += 1
+  }
+
+  // find the number of zeros for each multiplier
+  let multiplier_zeros = []
+  for ( let key in multipliers ){
+    let num = number
+    let zeros = 0
+    while(num > 0){
+      num =  parseInt(num / parseInt(key))
+      zeros += num
+    }
+    zeros = parseInt(zeros / multipliers[key])
+    multiplier_zeros.push(zeros)
+  }
+
+  return Math.min(...multiplier_zeros)
 }
